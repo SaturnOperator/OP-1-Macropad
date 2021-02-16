cec4194adce0a97fef51711c29a747e582079892
Abdullah Mostafa <17735907+MoonOperator@users.noreply.github.com>
2021-02-15 21:50:59 -0500

    Removing Mido library, interfacing directly with rtmidi library to be more efficient.
    Bare bones code to connect to OP-1 and read input stream on the byte level.

--- op1_macropad.py
+++ op1_macropad.py
@@ -1,16 +1,28 @@
-import mido
+from rtmidi import MidiIn as rtMidiIn
 
-op1 = mido.open_input('OP-1 Midi Device')
+# Get midi input
+midiInput = rtMidiIn()
 
-# Receiving bits & print data
-print("op1 receive():")
-bits = int.from_bytes(op1.receive().bin(), 'big')
-print(hex(bits))
-print()
+# Find OP-1 port
+# ValueError: 'OP-1 Midi Device' is not in list
+try:
+	op1Port = midiInput.get_ports().index('OP-1 Midi Device')
+except ValueError:
+	print("Could not find OP-1")
+	exit()
 
-print("command")
-print(hex((bits & 0xff0000) >> 0x10))
-print("id")
-print(hex((bits & 0xff00) >> 0x8))
-print("value")
-print(hex((bits & 0xff)))
+# Connect to OP-1 for midi input
+midiInput.open_port(op1Port)
+print("Connected to %s on port %d" % (midiInput.get_port_name(op1Port), op1Port))
+
+# Display first 10 actions
+count = 0
+while count < 10:
+	message = midiInput.get_message()
+	if(message):
+		print(message[0])
+		count += 1
+
+# Close connection
+midiInput.close_port()
+print("Closed connection on port %d" % op1Port)
