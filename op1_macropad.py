db5a49ab7459cb01dd02b0b66a74346339a7bc5d
Abdullah Mostafa <17735907+MoonOperator@users.noreply.github.com>
2021-02-15 23:09:47 -0500

    Truned OP-1 into a class

--- op1_macropad.py
+++ op1_macropad.py
@@ -1,28 +1,37 @@
 from rtmidi import MidiIn as rtMidiIn
 
-# Get midi input
-midiInput = rtMidiIn()
+class OP1(rtMidiIn):
+	def __init__(self):
+		# Connect to OP-1 for midi input
+		if(self.find_port()):
+			self.open_port(self.port)
+			print("Connected to %s on port %d" % (self.get_port_name(self.port), self.port))
 
-# Find OP-1 port
-# ValueError: 'OP-1 Midi Device' is not in list
-try:
-	op1Port = midiInput.get_ports().index('OP-1 Midi Device')
-except ValueError:
-	print("Could not find OP-1")
-	exit()
+	def find_port(self):
+		# Find the port that the OP-1 is on
+		try:
+			self.port = self.get_ports().index('OP-1 Midi Device')
+			return 1
+		except ValueError:
+			print("Could not find OP-1")
+			exit()
 
-# Connect to OP-1 for midi input
-midiInput.open_port(op1Port)
-print("Connected to %s on port %d" % (midiInput.get_port_name(op1Port), op1Port))
+	def query(self):
+		count = 0
+		while count < 10:
+			message = self.get_message()
+			if(message):
+				print(message[0])
+				count += 1
 
-# Display first 10 actions
-count = 0
-while count < 10:
-	message = midiInput.get_message()
-	if(message):
-		print(message[0])
-		count += 1
+	def close(self):
+		print("Closed connection on port %d" % self.port)
+		self.close_port()
 
-# Close connection
-midiInput.close_port()
-print("Closed connection on port %d" % op1Port)
+op1 = OP1()
+op1.query()
+op1.close()
+exit()
+
+
+
