8484c24ef6b09f8e3719e817a4d8c90db7411af6
Abdullah Mostafa <17735907+MoonOperator@users.noreply.github.com>
2021-02-15 20:01:33 -0500

    Receive bits from MIDI device

--- op1_macropad.py
+++ op1_macropad.py
@@ -2,7 +2,15 @@ import mido
 
 op1 = mido.open_input('OP-1 Midi Device')
 
-print(dir(op1))
+# Receiving bits & print data
+print("op1 receive():")
+bits = int.from_bytes(op1.receive().bin(), 'big')
+print(hex(bits))
+print()
 
-while(1):
-	print(op1.receive())
+print("command")
+print(hex((bits & 0xff0000) >> 0x10))
+print("id")
+print(hex((bits & 0xff00) >> 0x8))
+print("value")
+print(hex((bits & 0xff)))
